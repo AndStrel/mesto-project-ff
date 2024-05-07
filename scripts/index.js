ee2dccb74867f495c0151568156b549ed9f5b92c
AndStrel <xoz9ih@outlook.com>
2024-05-07 16:36:08 +0300

    Подготовил к ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,19 @@
-// @todo: Темплейт карточки
-console.log('Привет!');
-// @todo: DOM узлы
-
-// @todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+const cardTemplate = document.querySelector("#card-template").content;
+const placesList = document.querySelector(".places__list");
+initialCards.forEach(element => {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  cardElement.querySelector(".card__title").textContent = element.name;
+  cardElement.querySelector(".card__image").src = element.link;
+  cardElement.querySelector(".card__image").alt = element.name;
+  cardElement
+    .querySelector(".card__delete-button")
+    .addEventListener("click", evt => {
+      evt.target.parentElement.remove();
+    });
+  placesList.append(cardElement);
+});
+// @todo: Темплейт карточки V
+// @todo: DOM узлы V
+// @todo: Функция создания карточки V
+// @todo: Функция удаления карточки V
+// @todo: Вывести карточки на страницу V
