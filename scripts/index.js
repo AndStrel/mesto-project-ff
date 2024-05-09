e28dc685ec8a9a1408943000c0a9e214a053f2fe
AndStrel <xoz9ih@outlook.com>
2024-05-09 16:14:31 +0300

    Поправил после 2го ревью

--- scripts/index.js
+++ scripts/index.js
@@ -2,19 +2,23 @@ const cardTemplate = document.querySelector("#card-template").content;
 const placesList = document.querySelector(".places__list");
 
 function createCard(element, handleDelete) {
-  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  const cardElement = cardTemplate.querySelector(".places__item").cloneNode(true);
   const cardTitle = cardElement.querySelector(".card__title");
   const cardImage = cardElement.querySelector(".card__image");
   cardTitle.textContent = element.name;
   cardImage.src = element.link;
   cardImage.alt = element.name;
   const deleteButton = cardElement.querySelector(".card__delete-button");
-  function handleDelete() {
-    const placesItem = deleteButton.closest(".places__item");
-    placesItem.remove();
-  };
   deleteButton.addEventListener("click", handleDelete);
-  placesList.append(cardElement);
+    deleteButton.addEventListener("click", () => {
+    const placesItem = deleteButton.closest('.places__item');
+    placesItem.remove();});
   return cardElement;
 }
-initialCards.forEach(createCard);
+function handleDelete(evt) {
+  const placesItem = evt.target.closest(".places__item");
+  placesItem.remove();
+}
+initialCards.forEach((element) => {
+  placesList.append(createCard(element, handleDelete));
+});
